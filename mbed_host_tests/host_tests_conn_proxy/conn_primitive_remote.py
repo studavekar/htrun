6ff4caf5e8d124210ad0547e9678c57179e2c7e9
Shrikant Tudavekar <studavekar@gmail.com>
2017-09-14 17:39:58 -0500

    Prevent duplicate flash

--- mbed_host_tests/host_tests_conn_proxy/conn_primitive_remote.py
+++ mbed_host_tests/host_tests_conn_proxy/conn_primitive_remote.py
@@ -76,7 +76,7 @@ class RemoteConnectorPrimitive(ConnectorPrimitive):
         # Remote DUT connection, flashing and reset...
         try:
             #self.__remote_disconnect()  # we are not connected to call a disconnect
-            self.__remote_flashing(self.image_path, forceflash=True)
+            self.__remote_flashing(self.image_path, forceflash=False)
             self.__remote_connect(baudrate=self.baudrate)
             self.__remote_reset()
         except Exception as e:
